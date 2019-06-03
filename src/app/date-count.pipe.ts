d562e8b350ee2ccb0df3d48429fdf9f50f2c2dbe
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-03 10:24:39 +0300

    defining how many seconds are there in a day using const secondsInADay=86400

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,8 +7,8 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
       let today:Date = new Date(); 
       let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-      var dateDifference =Math.abs(value-today WithNoTime )
-      const secondsInADay= 86400; 
+      var dateDifference =Math.abs(value-todayWithNoTime )
+      const secondsInADay= 864 00; 
 
       var dateDifferenceSeconds=dateDifference*0.001; 
 
