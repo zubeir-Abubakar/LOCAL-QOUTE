61642f7ef0bb475a75fba31ed8df544887dce927
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-03 10:23:35 +0300

    value-todayWithNoTime in date count pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,7 +7,7 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
       let today:Date = new Date(); 
       let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-      var dateDifference =Math.abs(value-todayWithNoTime )
+      var dateDifference =Math.abs(value-today WithNoTime )
       const secondsInADay= 86400; 
 
       var dateDifferenceSeconds=dateDifference*0.001; 
