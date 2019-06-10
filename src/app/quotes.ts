56ac7e5ac0285ad0f2944a53bb7d52ff97510b2e
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-10 12:16:25 +0300

    Auto-generated commit

--- src/app/quotes.ts
+++
@@ -1,6 +0,0 @@
-export class Quotes {
-    public showDescription:boolean
-     constructor(public id:number, public name:string,public description:string,public postDate:Date){
-         this.showDescription=false
-  }
-  }
